a665016876b7478ff4db17f6d7496cdaf20c7a31
Jordan Jalabert <jordan.jalabert@gmail.com>
2024-05-28 16:46:52 +0200

    ongoing changes on rental controllers

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,4 +1,16 @@
 class RentalsController < ApplicationController
+
+  def create
+    @rental = Rental.new(rental_params)
+    @rental.user = current_user
+    @rental.bike
+    if @rental.save
+      redirect_to dashboard_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def accepted
     @rental = Rental.find(params[:id])
     @rental.status = "accepted"
@@ -9,5 +21,11 @@ class RentalsController < ApplicationController
     @rental = Rental.find(params[:id])
     @rental.status = "declined"
     redirect_to dashboard_path, notice: "Rental updated successfully."
+  end
+
+  private
+
+  def rental_params
+    params.require(:bike).permit(:start_date, :end_date)
   end
 end
