5e91fe60ca7c5e6ad1f775911560d9faaa80a0bc
bastien penard <bastien.penard@gmail.com>
2024-05-29 11:48:57 +0200

    Fixing merge conflits

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,16 @@ Rails.application.routes.draw do
     resources :rentals, only: %i[update create]
   end
 
+  get 'owner_dashboard', to: "pages#owner_dashboard"
+  get 'renter_dashboard', to: "rentals#renterdashboard"
+end
+
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "pages#home"
+  resources :bikes do
+    resources :rentals, only: %i[update create]
+  end
 
   get 'owner_dashboard', to: "pages#owner_dashboard"
   get 'renter_dashboard', to: "rentals#renter_dashboard"
