10410cf09f603838f1ccb3d118f148983dfaafd4
bastien penard <bastien.penard@gmail.com>
2024-05-31 11:27:09 +0200

    added the map on the bike show

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -22,7 +22,13 @@ class BikesController < ApplicationController
   def show
     @bike = Bike.find(params[:id])
     @rental = Rental.new
-
+    @user = @bike.user
+    @markers = [{
+        lat: @user.latitude,
+        lng: @user.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {user: @user}),
+        marker_html: render_to_string(partial: "marker")
+      }]
   end
 
   def create
